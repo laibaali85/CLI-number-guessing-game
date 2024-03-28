b2a23b5f5ecf59657cfdf6b9759312980c2a5d02
laibaali85 <laibaali858452@gmail.com>
2024-03-28 10:29:02 -0700

    number guessing game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CLI-number-guessing-game
 this is a simple cli-number guessing game
+
+
+npx laibay-cli-number-guessing-game
---
+++ index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+// steps of making a project:-
+//1) computer will generate a random number;
+//2) user input for guessing number:
+//3) computer user input with computer generaated number and show result: 
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: " please guess a number between 1-10",
+    }]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guesses the right number");
+}
+else {
+    console.log("you guess the wrong number");
+}
+;
